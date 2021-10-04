9fe866c198f9e9566cea04bd7e1ac925778dc191
qixinquan <1198316737@qq.com>
2021-10-04 20:34:40 +0800

    分支操作

--- src/test/java/com/offcn/DemoApplicationTests.java
+++ src/test/java/com/offcn/DemoApplicationTests.java
@@ -12,6 +12,7 @@ class DemoApplicationTests {
         System.out.println("测试dev2");
        System.out.println("dev2");
         System.out.println("dev2第二次推送");
+        System.out.println("第三次推送前，已经有人推送过");
 
     }
 
