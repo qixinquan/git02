fe693a3a8576548925031aced0109367bb14b11f
qixinquan <61838512+qixinquan@users.noreply.github.com>
2021-10-04 20:48:26 +0800

    Update DemoApplicationTests.java

--- src/test/java/com/offcn/DemoApplicationTests.java
+++ src/test/java/com/offcn/DemoApplicationTests.java
@@ -10,12 +10,11 @@ class DemoApplicationTests {
     void contextLoads() {
         System.out.println("测试");
         System.out.println("测试dev2");
-       System.out.println("dev2");
+        System.out.println("dev2");
         System.out.println("dev2第二次推送");
         System.out.println("第三次推送前，已经有人推送过");
         System.out.println("dev2第三次次推送，冲突解决");
-
-
+        System.out.println("dev2第四次次推送，冲突解决");  
     }
 
 }
